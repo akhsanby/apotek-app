5051db5157dd872a1c7042796e2029a10b9232a5
akhsanby <49837708+AkhsanBy@users.noreply.github.com>
2021-02-22 16:46:38 +0700

    Update DatabaseSeeder.php

--- app/Database/Seeds/DatabaseSeeder.php
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -32,22 +32,5 @@ class DatabaseSeeder extends Seeder
 				'icon' => 'default.png'
 			]);
 		}
-
-		for ($i = 0; $i < 10; $i++) {
-			$this->db->table('transaksi')->insert([
-		        'id_user' => 1, // admin
-		        'nama_pembeli' => static::faker()->word,
-		        'tgl_transaksi' => static::faker()->dateTime()->format('Y-m-d'),
-		    ]);
-		}
-
-		for ($i = 0; $i < 10; $i++) {
-			$this->db->table('detil_transaksi')->insert([
-		        'kode_transaksi' => static::faker()->numberBetween(1, 10),
-		        'kode_obat' => static::faker()->numberBetween(1, 10),
-		        'sub_total' => static::faker()->numberBetween(2, 10),
-		        'total' => static::faker()->numberBetween(2, 10) * 1000,
-		    ]);
-		}
 	}
 }
